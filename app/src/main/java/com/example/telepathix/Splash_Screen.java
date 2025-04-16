afa7d9b30185ba6f4066c4cdff780572cb5ed702
marcRanielle <phorabanillo@gmail.com>
2025-04-16 21:56:15 +0800

    Initial Commit

--- app/src/main/java/com/example/telepathix/Splash_Screen.java
+++ app/src/main/java/com/example/telepathix/Splash_Screen.java
@@ -11,7 +11,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class Splash_Screen extends AppCompatActivity {
 
-    private static final int SPLASH_TIME_OUT = 2000;
+    private static final int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
